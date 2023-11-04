7344f989b6621ac066a0e703beceecec7b51ae28
Yu Zhang <yuzhang94@outlook.com>
2023-11-04 13:06:16 +0800

    build(vite): add url base

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import path from 'node:path'
+import process from 'node:process'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import typescript2 from 'rollup-plugin-typescript2'
@@ -6,6 +7,7 @@ import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/VObjectInspector/' : '/',
   plugins: [
     vue(),
     dts({
